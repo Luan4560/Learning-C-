dd1d26e088ff19d6a1401c9ed71dd157cb1d3589
Luan Nascimento <luan.nascimento11@outlook.com>
2020-08-12 22:53:24 -0300

    if, loops, while, doWhile and for

--- Program.cs
+++ Program.cs
@@ -12,16 +12,13 @@ namespace myapp
             int c = a + b;
             int d = a + b * c;
             Console.WriteLine(d);
-            //Subtração
-            // Console.WriteLine(a - b);
-            //Multiplicação
-            // Console.WriteLine(a * b);
-            //Divisão
-            // Console.WriteLine(a / b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
             
         }
 
-          static void OrderPrecedence()
+        static void OrderPrecedence()
         {
             int a = 5;
             int b = 4;
@@ -62,13 +59,127 @@ namespace myapp
             Console.WriteLine($"An example of overflow: {what}");
         }
 
+        static void TestDoubles()
+        {
+        double a = 19;
+        double b = 23;
+        double c = 8;
+        double d = (a + b) / c;
+        Console.WriteLine(d);
+
+        double max = double.MaxValue;
+        double min =double.MinValue;
+        Console.WriteLine($"The range of double is {min} to {max}");
+
+        double third = 1.0 / 3.0;
+        Console.WriteLine(third);
+        }
+
+        static void TestDecimal()
+        {
+            decimal min = decimal.MinValue;
+            decimal max = decimal.MaxValue;
+            Console.WriteLine($"The range of decimal types is {min} to {max}");
+            
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine(a / b);
+
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine(c / d);
+
+        }
+
+        static void Challenge()
+        {
+            double circulo = 2.50;
+            double result = (circulo * circulo) * Math.PI;
+            double anotherResut = Math.Pow(circulo, 2) * Math.PI;
+            Console.WriteLine(result);
+            Console.WriteLine(anotherResut);
+        } 
+
+        static void LearIf ()
+        {
+            int a = 5;
+            int b = 6;
+            int c = 4;
+            if((a + b + c > 10) || (a == b)) 
+            {
+                Console.WriteLine("The answer is greater than 10.");
+                Console.WriteLine("And the first number is equal to the second.");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("Or the first number is not equal to the second");
+            }
+
+        }
+
+        static void While ()
+        {
+            int counter = 0;
+            do
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            } while (counter < 10);
+        }
+
+        static void For () 
+        {
+            for (int index = 5; index < 16; index ++)
+            {
+                Console.WriteLine($"Hello World! The index is {index}");
+            }
+        }
+
+        static void Matriz ()
+        {
+            for(int row = 1; row < 11; row++)
+            {
+            Console.WriteLine(row);
+            }
+
+            for(char column = 'a'; column < 'k'; column++)
+            {
+                Console.WriteLine($"The column is {column}");
+            }
+
+           
+            for (char column = 'a'; column < 'k'; column++)
+            {
+                for (int row = 1; row < 11; row++)
+            {
+                Console.WriteLine($"The cell is ({row}, {column})");
+            }
+            }
+        }
+
         static void Main(string[] args)
         {
-            WorkingWithIntegers();
+            // WorkingWithIntegers();
 
-            OrderPrecedence();
+            // OrderPrecedence();
 
-            TestLimits();
+            // TestLimits();
+
+            // TestDoubles();
+
+            // TestDecimal();
+
+            // Challenge();
+
+            // LearIf();
+            
+            // While ();
+            
+            // For (); 
+
+            Matriz ();
+
 
         }
     }
